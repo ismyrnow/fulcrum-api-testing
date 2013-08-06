70e79e2ae05c97790cfa21551392b540a55797df
Tyler Garner <garnertb@gmail.com>
2013-08-06 16:56:30 -0400

    Minor changes to help testing.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 fulcrum-api-testing
 ===================
 
-Quick app testing the fulcrum API.
+Quick app for locally testing the fulcrum API.  Running on a public facing server will expose your fulcrum API key.
+
+Installation
+============
+
+Run as static files on any web server.
+
+api_key.js
+----------
+Add your api key in js/api_key.js, for example:
+
+```
+api _key = "<your api_key>";
+```
--- js/fulcrum.js
+++ js/fulcrum.js
@@ -1,11 +1,14 @@
+//requires jQuery, Leaflet
+
 var fulcrum = {};
 
 fulcrum.options = {
     'apiKey':'',
+    'apiURL': "https://api.fulcrumapp.com/api/v2/",
+    'debug': true,
     'mapDiv':'map',
     'mapCenter': [37.52,-77.43],
-    'mapZoom': 11,
-    'apiURL': "https://api.fulcrumapp.com/api/v2/"
+    'mapZoom': 11
 };
 
 fulcrum.fulcrumappRequest = function(restNoun, callback){
@@ -29,12 +32,24 @@ fulcrum.init=function(options){
     });
 
     fulcrum.map = fulcrum.setUpMap();
-    fulcrum.get_users();
+
+    if (fulcrum.options.debug){
+        fulcrum.get_records_debug();
+    }else{
+        fulcrum.get_records();
+    }
+
 }
 
 fulcrum.isFulcrumRequest = function(url){
     //return true if request is going to fulcrumapp.com else fasle
-    return url.split('//')[1].substring(0, 18) == "api.fulcrumapp.com";
+    domain =  url.split('//')[1]
+    if (domain){
+        return domain.substring(0, 18) == "api.fulcrumapp.com";
+    } else {
+        return false
+    }
+
 }
 
 fulcrum.setUpMap = function(){
@@ -44,11 +59,42 @@ fulcrum.setUpMap = function(){
     return map
 }
 
-fulcrum.parseUsers = function(users){
-    console.log(users);
-}
-
 fulcrum.get_users = function(){
     return fulcrum.fulcrumappRequest('users', fulcrum.parseUsers);
 }
 
+fulcrum.get_records = function(){
+    fulcrum.fulcrumappRequest('records', fulcrum.parseRecords);
+}
+
+fulcrum.get_records_debug = function(){
+    $.ajax({
+        url: 'sources/records.json',
+        type: "GET",
+        success: fulcrum.parseRecords
+    });
+}
+
+//parsing callbacks
+
+fulcrum.parseUsers = function(users){
+    console.log(users);
+}
+
+fulcrum.buildPopup = function (record){
+    html = "<h4>" + record.id + "</h4><br/>";
+    html += "<b>Project: </b>" + record.project_id + "</br>";
+    html += "<b>Last updated: </b>" + record.client_updated_at + "</br>";
+    return html
+}
+
+fulcrum.parseRecords = function(page){
+    total_records = page.total_count;
+    $('#hud').text('Number of Records: ' + total_records);
+
+    $.each(page.records, function(n, record){
+        console.log(record);
+        incident = L.marker([record.latitude, record.longitude]).addTo(fulcrum.map)
+            .bindPopup(fulcrum.buildPopup(record));
+    });
+}
